b7e2e405f419cd39ecba8065c939e505e89b503d
Brian Wynn <brian.wynn83@gmail.com>
2015-04-21 00:56:18 -0700

    Set styles up so that the gallery slides are all on the same line, and put in some very basic anticipatory js object and anonymous function. I'll surely wind up deleting them when all is said and done.

---
+++ script.js
@@ -0,0 +1,9 @@
+var slide = function() {
+  var windowWidth = window.innerWidth;
+  return windowWidth;
+}
+
+var gallerySlider = (function() {
+  var showNext = document.querySelectorAll('#right > a');
+  var showPrev = document.querySelectorAll('#left > a');
+})();
