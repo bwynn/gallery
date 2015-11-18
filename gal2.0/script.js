823e2874d44b7aa74ffa6f8dd24254999b70e627
Brian Wynn <brian.wynn83@gmail.com>
2015-11-17 23:50:43 -0800

    Migrated the current slide function into the constructor function to create one object for api ease and accessibility.

--- gal2.0/script.js
+++ gal2.0/script.js
@@ -10,65 +10,99 @@
 //    - create/build slides
 //    - touch navigation
 
-var slides = [];
+var slides = []; // slides array stores slide objects, passed in using api listed
+// below
 
 function Slide() {
-  this.name = name;
-  this.active = false; // default state
-};
+  this.name; // name property
+  this.active = false; // default state - false, this property will act as the
+  // state switcher for the current gallery slide in the gallery.
+}
 
 function Gallery() {
 
   this.addSlide = function( name, path, arr ) {
-    var slide = new Slide();
-    slide.name = name;
-    slide.src = path;
-    arr.push( slide );
+    var slide = new Slide(); // invoke new slide object
+    slide.name = name; // get name as a string
+    slide.src = path; // get url path as a string
+    arr.push( slide ); // push the slide object into the slides array
+
+    console.log(arr);
   };
 
   // implemented as Gallery.defaultState.call(this, arr);
-  this.defaultActiveState = function( arr ) {
+  this.defaultSlideState = function( arr ) {
     // set all active state values to false
     for ( var i = 0; i < arr.length; i++ ) {
       arr[i].active = false;
     }
     // return with the first slide as active
-    return arr[0].active = true;
+    arr[0].active = true;
+    console.log(arr);
+  };
+
+  // curSlide takes the gallery array object as a parameter, which controls
+  // session states and changes
+  this.currentSlide = function( arr ) {
+    var selected; // declare selected
+    for ( var i = 0; i < arr.length; i++ ) {
+      if ( arr[i].active === true ) { // if the slide object has a property with the active state === true
+        selected = arr[i]; // set selected var to the current slide to create a
+      }                    // session instance
+    }
+
+    function index() {
+      return arr.indexOf( selected ); // set var for the current index value
+    }
+
+    return {
+      selected: selected,
+      index: index
+    };
+  };
+
+  // increment/advance to the next array object
+  this.advanceIndex = function() {
+    // if the index value is less than or equal to the array length value
+    if ( index < arr.length - 1 ) {
+      var newIdx = index += 1;
+      // clear out current active state
+      selected.active = false;
+
+      // assign new active state index
+      arr[newIdx].active = true;
+      console.log(arr);
+    }
+    else {
+      console.log("Last slide in the array");
+    }
+  };
+
+  this.previousIndex = function() {
+    if (index >= 1) {
+      var newIdx = index -= 1;
+
+      // clear out active state
+      selected.active = false;
+
+      // new index assigned
+      arr[newIdx].active = true;
+      console.log(arr);
+    }
+    else {
+      return console.log("At the first slide, can't go back any further.");
+    }
   };
 
   return {
     addSlide: this.addSlide,
-    defaultState: this.defaultActiveState
+    defaultSlideState: this.defaultSlideState,
+    currentSlide: this.currentSlide,
+    advanceIndex: this.advanceIndex,
+    previousIndex: this.previousIndex
   };
-};
+}
 
 var gallery = new Gallery(); // create new gallery object
 
-// gallery().addSlide("slide 1", "imgsrc.jpg", slides); // invocation of a new slide
-
-function curSlide(arr) {
-  var selected;
-  for ( var i = 0; i < arr.length; i++ ) {
-    if ( arr[i].active === true ) {
-      selected = arr[i];
-    }
-  }
-  var index = arr.indexOf( selected );
-
-  // increment to the next index
-  this.advance = function() {
-    var newIdx = index += 1;
-    // clear out current active state
-    selected.active = false;
-
-    // assign new active state index
-    arr[newIdx].active = true;
-    console.log( arr[newIdx] );
-  }
-
-  return {
-    selected: selected,
-    index: index,
-    advance: this.advance
-  }
-}
+// gallery.addSlide("slide 1", "imgsrc.jpg", slides); // invocation of a new slide
