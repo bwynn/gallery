7fc41fccff7b1de1ffcc9677202c270b59f04604
Brian Wynn <b_wynn@apple.com>
2015-11-17 17:04:32 -0800

    Updated the api to include increasing index function.

--- gal2.0/script.js
+++ gal2.0/script.js
@@ -19,23 +19,56 @@ function Slide() {
 
 function Gallery() {
 
-  function addSlide( name, src, arr ) {
+  this.addSlide = function( name, path, arr ) {
     var slide = new Slide();
     slide.name = name;
-    slide.src = src;
+    slide.src = path;
     arr.push( slide );
   };
 
-  function listSlides( arr ) {
-    console.log( arr );
-  }
+  // implemented as Gallery.defaultState.call(this, arr);
+  this.defaultActiveState = function( arr ) {
+    // set all active state values to false
+    for ( var i = 0; i < arr.length; i++ ) {
+      arr[i].active = false;
+    }
+    // return with the first slide as active
+    return arr[0].active = true;
+  };
 
   return {
-    addSlide: addSlide,
-    listSlides: listSlides
+    addSlide: this.addSlide,
+    defaultState: this.defaultActiveState
   };
 };
 
-var gallery = new Gallery();
+var gallery = new Gallery(); // create new gallery object
 
 // gallery().addSlide("slide 1", "imgsrc.jpg", slides); // invocation of a new slide
+
+function curSlide(arr) {
+  var selected;
+  for ( var i = 0; i < arr.length; i++ ) {
+    if ( arr[i].active === true ) {
+      selected = arr[i];
+    }
+  }
+  var index = arr.indexOf( selected );
+
+  // increment to the next index
+  this.advance = function() {
+    var newIdx = index += 1;
+    // clear out current active state
+    selected.active = false;
+
+    // assign new active state index
+    arr[newIdx].active = true;
+    console.log( arr[newIdx] );
+  }
+
+  return {
+    selected: selected,
+    index: index,
+    advance: this.advance
+  }
+}
